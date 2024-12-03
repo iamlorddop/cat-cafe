8ed9ca7bd60fd064b08ea8cba1e7389edda3d3f3
iamlorddop <juliahavaeva7@gmail.com>
2024-12-03 21:08:47 +0300

    feat: add iconCls, buttonType, selected props to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,41 @@
 import { ButtonHTMLAttributes, FC } from "react";
 import clsx from "clsx";
 
+import { Icon } from "../Icon";
+
 import styles from "./Button.module.css";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   /** Текст кнопки */
-  text: string;
+  text?: string;
+  /** Тип кнопки. По умолчанию "primary" */
+  buttonType?: "primary" | "light";
+  /** Кнопка выбрана */
+  selected?: boolean;
+  /** Класс иконки */
+  iconCls?: string;
 }
 
 /** Компонент кнопка */
-export const Button: FC<ButtonProps> = ({ text, className, ...props }) => {
-  const buttonCls = clsx(styles.button, className);
+export const Button: FC<ButtonProps> = ({
+  text,
+  iconCls,
+  buttonType = "primary",
+  selected,
+  className,
+  ...props
+}) => {
+  const buttonCls = clsx(
+    styles.button,
+    styles[buttonType],
+    iconCls && buttonType !== "light" && styles.buttonWithIcon,
+    selected && styles.buttonSelected,
+    className
+  );
   return (
     <button className={buttonCls} {...props}>
-      {text}
+      {!iconCls && text && text}
+      {iconCls && <Icon className={iconCls} size="m" />}
     </button>
   );
 };
